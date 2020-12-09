94b995586e8710d530a339bb060d727808b17477
superoft1 <superoft1@users.noreply.github.com>
2020-12-09 20:21:35 +0700

    add database

--- WeSplitProject/obj/Debug/WeSplitProject_Content.g.i.cs
+++ WeSplitProject/obj/Debug/WeSplitProject_Content.g.i.cs
@@ -8,6 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/images/sora.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("wesplitdb.context.tt")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("wesplitdb.tt")]
 
 
