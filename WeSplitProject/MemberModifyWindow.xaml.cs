038fc12b7a0d556f87266483804567cf147080d9
superoft1 <superoft1@users.noreply.github.com>
2020-12-10 22:52:22 +0700

    update NewTrip Form

---
+++ WeSplitProject/MemberModifyWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WeSplitProject
+{
+    /// <summary>
+    /// Interaction logic for MemberModifyWindow.xaml
+    /// </summary>
+    public partial class MemberModifyWindow : Window
+    {
+        public MemberModifyWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void avatarImage_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void avatarImage_Drop(object sender, DragEventArgs e)
+        {
+
+        }
+
+
+        private void deleteExpenseBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void addExpenseBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WeSplitProject/TripEditWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using MaterialDesignThemes;
+using MaterialDesignColors;
+
+namespace WeSplitProject
+{
+    /// <summary>
+    /// Interaction logic for CreateNewTrip.xaml
+    /// </summary>
+    public partial class CreateNewTrip : Window
+    {
+        public CreateNewTrip()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TripImage_Drop(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void TripImage_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void addExpenseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //Horizontal Stack panel
+            StackPanel subExpenseSP = new StackPanel();
+            subExpenseSP.Orientation = Orientation.Horizontal;
+
+            //Description textbox
+            TextBox expenseDesTextBox = new TextBox();
+            expenseDesTextBox.Width = 180;
+            expenseDesTextBox.FontSize = 16;
+            expenseDesTextBox.Margin = new Thickness(0, 10, 5, 0);
+            MaterialDesignThemes.Wpf.HintAssist.SetHint(expenseDesTextBox, "Mô tả");
+
+            //Cost textbox
+            TextBox CostTextBox = new TextBox();
+            CostTextBox.Width = 100;
+            CostTextBox.FontSize = 16;
+            CostTextBox.Margin = new Thickness(5, 10, 0, 0);
+            MaterialDesignThemes.Wpf.HintAssist.SetHint(CostTextBox, "Chi phí");
+
+            subExpenseSP.Children.Add(expenseDesTextBox);
+            subExpenseSP.Children.Add(CostTextBox);
+
+            SPExpense.Children.Add(subExpenseSP);
+        }
+
+        private void addVisitLocBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void deleteVisitLocBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void addMemberBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void deleteMemberBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void addPaymentLocBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
