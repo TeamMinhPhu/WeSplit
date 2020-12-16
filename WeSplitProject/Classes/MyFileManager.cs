06d2b5ed7b6858e0aab0365cbba2380c8d33db46
superoft1 <superoft1@users.noreply.github.com>
2020-12-16 11:59:45 +0700

    update

--- WeSplitProject/Classes/MyFileManager.cs
+++ WeSplitProject/Classes/MyFileManager.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Windows.Media.Imaging;
 
 namespace WeSplitProject.Classes
 {
@@ -43,8 +44,30 @@ namespace WeSplitProject.Classes
         {
             if (File.Exists(filepath))
             {
+                System.GC.Collect();
+                System.GC.WaitForPendingFinalizers();
                 File.Delete(filepath);
             }
+        }
+
+        public static bool IsImageFile(string fileName)
+        {
+            string targetExtension = System.IO.Path.GetExtension(fileName);
+            bool result = false;
+            if (!String.IsNullOrEmpty(targetExtension))
+            {
+                List<string> recognisedImageExtensions = new List<string>() { ".jpg", ".jpeg", ".gif", ".png", ".bmp", ".tiff", ".ico" };
+                foreach (string extension in recognisedImageExtensions)
+                {
+                    if (extension.Equals(targetExtension))
+                    {
+                        result = true;
+                        break;
+                    }
+                }
+            }
+            else { /*do nothing*/ }
+            return result;
         }
     }
 }
